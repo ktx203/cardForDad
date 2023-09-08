97f993b4d2116e65030f331136bdfcdf35125131
kyle <kylexia.tl@gmail.com>
2023-09-08 15:18:57 -0400

    removed none

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let cardEl = document.getElementById("cardId");
 let linkEl = document.getElementById("galleryLink")
 let btnPress = [false,false,false,false,false];
 
-linkEl.style.display = "none";
 cardEl.addEventListener("click", call);
 function call() {
     if (cardEl.className === "card"){
