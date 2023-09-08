1bf5097f97d41e5d5cf69c399945bacbf1d993b5
kyle <kylexia.tl@gmail.com>
2023-09-08 14:44:14 -0400

    gallery link hidden until all gifts open

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var cardEl = document.getElementById("cardId");
+let cardEl = document.getElementById("cardId");
+let linkEl = document.getElementById("galleryLink")
+let btnPress = [false,false,false,false,false];
 
 cardEl.addEventListener("click", call);
 function call() {
@@ -23,38 +25,57 @@ const zuzuDad = document.getElementById("zuzuDad");
 buttonOne.addEventListener("click", function () {
     cardEl.className = "card"
     duoDad.style.display = "block";
+    btnPress[0] = true;
   });
 buttonTwo.addEventListener("click", function () {
     cardEl.className = "card"
     youngDad.style.display = "block";
+    btnPress[1] = true;
   });
 buttonThree.addEventListener("click", function () {
     cardEl.className = "card"
     babyDad.style.display = "block";
+    btnPress[2] = true;
   });
 buttonFour.addEventListener("click", function () {
     cardEl.className = "card"
     exploreDad.style.display = "block";
+    btnPress[3] = true;
   });
 buttonFive.addEventListener("click", function () {
     cardEl.className = "card"
     zuzuDad.style.display = "block";
+    btnPress[4] = true;
   });
   
 
 duoDad.addEventListener("click", function () {
     duoDad.style.display = "none";
+    checkBtnPress();
   });
 youngDad.addEventListener("click", function () {
     youngDad.style.display = "none";
+    checkBtnPress();
   });
 babyDad.addEventListener("click", function () {
     babyDad.style.display = "none";
+    checkBtnPress();
   });
 exploreDad.addEventListener("click", function () {
     exploreDad.style.display = "none";
+    checkBtnPress();
   });
 zuzuDad.addEventListener("click", function () {
     zuzuDad.style.display = "none";
+    checkBtnPress();
   });
   
+  function checkBtnPress () {
+       if (btnPress.every(checkTrue))
+        linkEl.style.display = "block"
+      
+  }
+
+  function checkTrue(btnPress) {
+    return btnPress === true;
+  }
